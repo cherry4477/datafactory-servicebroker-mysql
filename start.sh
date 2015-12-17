6cb8996efd67bf7737e8da897cd963f24b68b2f8
panxy <1851616111@qq.com>
2015-12-17 19:15:03 +0800

    refactor path

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export MYSQL_ADDR=192.168.1.119
 export MYSQL_PORT=3306
 export MYSQL_DATABASE=mysql
 export MYSQL_USER=root
-export MYSQL_PASSWD=root
+export MYSQL_ENV_MYSQL_ROOT_PASSWORD=root
 
 go build
 ./broker_mysql

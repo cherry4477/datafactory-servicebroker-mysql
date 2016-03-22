2985877a8d675d6d97a84d32a2e09b143f9d55ae
1851616111 <1851616111@qq.com>
2016-03-22 14:37:15 +0800

    test

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export MYSQL_ADDR=54.222.153.228
-export MYSQL_PORT=3306
+export MYSQL_PORT_3306_TCP_ADDR=10.1.236.121
+export MYSQL_PORT_3306_TCP_PORT=3306
 export MYSQL_DATABASE=mysql
 export MYSQL_USER=root
 export MYSQL_ENV_MYSQL_ROOT_PASSWORD=root
